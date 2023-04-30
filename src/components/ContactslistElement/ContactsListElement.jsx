11e51b3f4b133fb96b2ab8cd2c0e3a83e87b0beb
Rafał Gałecki <darthunter@gmail.com>
2023-04-30 20:29:38 +0200

    contacts list grid

--- src/components/ContactslistElement/ContactsListElement.jsx
+++ src/components/ContactslistElement/ContactsListElement.jsx
@@ -6,15 +6,18 @@ import PropTypes from 'prop-types';
 
 const ContactsListElement = ({ contact }) => {
   const dispatch = useDispatch();
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
 
   const handleClick = () => {
     dispatch(deleteContact(contact.id));
   };
 
   return (
-    <div className={css.contactLi}>
-      <span>&#10047;</span>
-      <span className={css.contact}>{contact.name}:</span>
+    <div className={css.contactLi} style={{ color: getRandomHexColor(), textShadow: '1px 1px black' }}>
+      <span className={css.contact}>&#10047; {contact.name}:</span>
       <span className={css.contact}>{contact.number}</span>
       <button type="button" className={css.btnDelete} onClick={handleClick}>
         &#9760;
