701e509c356c3c3e99de9b5d12a2c15c3454336a
Rafał Gałecki <darthunter@gmail.com>
2023-05-01 10:58:27 +0200

    colors

--- src/components/ContactslistElement/ContactsListElement.jsx
+++ src/components/ContactslistElement/ContactsListElement.jsx
@@ -10,15 +10,24 @@ const ContactsListElement = ({ contact }) => {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   }
 
-
   const handleClick = () => {
     dispatch(deleteContact(contact.id));
   };
 
   return (
-    <div className={css.contactLi} style={{ color: getRandomHexColor(), textShadow: '1px 1px black' }}>
-      <span className={css.contact}>&#10047; {contact.name}:</span>
-      <span className={css.contact}>{contact.number}</span>
+    <div
+      className={css.contactLi}
+      // style={{ color: getRandomHexColor(), textShadow: '1px 1px black' }}
+    >
+      <p className={css.contact}>
+        <span
+          style={{ color: getRandomHexColor(), textShadow: '1px 1px black' }}
+        >
+          &#10047;
+        </span>{' '}
+        {contact.name}:
+      </p>
+      <p className={css.contact}>{contact.number}</p>
       <button type="button" className={css.btnDelete} onClick={handleClick}>
         &#9760;
       </button>
